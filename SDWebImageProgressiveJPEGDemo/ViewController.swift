04e63927cde4f4509304dd86bfbeb6ef2be6b39f
Bogdan Poplauschi <bpoplauschi@gmail.com>
2017-10-25 18:33:04 +0300

    Initial Commit

---
+++ SDWebImageProgressiveJPEGDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SDWebImageProgressiveJPEGDemo
+//
+//  Created by Bogdan Poplauschi on 25/10/2017.
+//  Copyright © 2017 SDWebImage. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
